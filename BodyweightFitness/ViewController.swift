5440873ce7a9dc31819eb396117528b809f031ed
Tejaswi Yerukalapudi <tejaswi.yvs@gmail.com>
2015-07-22 13:41:14 -0400

    Initial Commit

---
+++ BodyweightFitness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BodyweightFitness
+//
+//  Created by Tejaswi Yerukalapudi on 7/22/15.
+//  Copyright © 2015 Tejaswi Yerukalapudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
